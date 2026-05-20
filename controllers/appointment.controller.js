7018887356c6953f0be54424b6d297dd7aafd854
Soumyadip Paul <140958717+SOUMYADIP461@users.noreply.github.com>
2026-05-20 12:12:04 +0530

    Update appointment.controller.js

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -2,72 +2,6 @@ const pool = require('../config/db');
 
 
 
-// exports.createAppointment = async (req, res) => {
-
-//     const { appointment_id, doctor_id, patient_id, company, appointment_date } = req.body;
-
-//     const conn = await pool.getConnection();
-
-//     try {
-
-//         await conn.beginTransaction();
-
-//         const [rows] = await conn.query(
-//             `SELECT COUNT(*) AS total
-//              FROM QueueTable
-//              WHERE doctor_id = ?
-//              AND company = ?
-//              AND appointment_date = ?`,
-//             [doctor_id, company, appointment_date]
-//         );
-
-//         const queue_no = rows[0].total + 1;
-
-//         await conn.query(
-//             `INSERT INTO QueueTable
-//              (appointment_id, doctor_id, patient_id, company, appointment_date, queue_no)
-//              VALUES (?, ?, ?, ?, ?, ?)`,
-//             [appointment_id, doctor_id, patient_id, company, appointment_date, queue_no]
-//         );
-
-//         await conn.commit();
-
-//         res.json({
-//             success: true,
-//             message: "Appointment created successfully",
-//             data: {
-//                 appointment_id,
-//                 doctor_id,
-//                 patient_id,
-//                 company,
-//                 appointment_date,
-//                 queue_no
-//             }
-//         });
-
-//     } catch (err) {
-
-//         await conn.rollback();
-
-//         if (err.code === 'ER_DUP_ENTRY') {
-//             return res.status(400).json({
-//                 success: false,
-//                 message: "Appointment ID already exists. Please send a unique appointment_id."
-//             });
-//         }
-
-//         return res.status(500).json({
-//             success: false,
-//             message: "Database error while creating appointment"
-//         });
-
-//     } finally {
-
-//         conn.release();
-
-//     }
-
-// };
 
 exports.createAppointment = async (req, res) => {
 
@@ -289,6 +223,65 @@ exports.getTotalPatientsByCompanyDate = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Database error while fetching total patients"
+        });
+
+    }
+};
+
+
+
+// patients details 
+exports.getQueueByDoctorDate = async (req, res) => {
+    const { doctor_id, company, appointment_date } = req.query;
+
+    if (!doctor_id || !company || !appointment_date) {
+        return res.status(400).json({
+            success: false,
+            message: "doctor_id, company and appointment_date are required"
+        });
+    }
+
+    try {
+
+        const [rows] = await pool.query(
+            `SELECT 
+                patient_id,
+                appointment_id,
+                queue_no
+             FROM QueueTable
+             WHERE doctor_id = ?
+             AND company = ?
+             AND appointment_date = ?
+             ORDER BY queue_no ASC`,
+            [doctor_id, company, appointment_date]
+        );
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No queue found"
+            });
+        }
+
+        return res.json({
+            success: true,
+            message: "Queue fetched successfully",
+            data: {
+                doctor_id,
+                company,
+                appointment_date,
+                total_patients: rows.length,
+                patients: rows
+            }
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        return res.status(500).json({
+            success: false,
+            message: "Database error while fetching queue"
         });
 
     }
